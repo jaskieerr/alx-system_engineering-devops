dbf2e39de6ca2cd06a11d55284419244360a4432
jaskieerr <nizar.zairat@gmail.com>
2024-05-20 16:00:12 -0700

    emlogdgdg

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,51 +1,14 @@
 #!/usr/bin/python3
-"""
-This script fetches and displays the TODO list progress of an employee
-based on their ID using a REST API.
-"""
-
+'''fetches employee stuff'''
 import requests
 import sys
 
-
-def get_employee_todo_list_progress(employee_id):
-    base_url = 'https://jsonplaceholder.typicode.com'
-    
-    # Get employee details
-    user_response = requests.get(f'{base_url}/users/{employee_id}')
-    if user_response.status_code != 200:
-        print(f"Employee with ID {employee_id} not found.")
-        return
-    
-    user_data = user_response.json()
-    employee_name = user_data['name']
-    
-    # Get employee's TODO list
-    todos_response = requests.get(f'{base_url}/todos?userId={employee_id}')
-    if todos_response.status_code != 200:
-        print(f"Could not retrieve TODO list for employee ID {employee_id}.")
-        return
-    
-    todos_data = todos_response.json()
-    total_tasks = len(todos_data)
-    done_tasks = [task for task in todos_data if task['completed']]
-    number_of_done_tasks = len(done_tasks)
-    
-    # Display the TODO list progress
-    print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
-    for task in done_tasks:
-        print(f"\t {task['title']}")
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./0-gather_data_from_an_API.py EMPLOYEE_ID")
-        sys.exit(1)
-    
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("Employee ID must be an integer.")
-        sys.exit(1)
-    
-    get_employee_todo_list_progress(employee_id)
+    url = "https://jsonplaceholder.typicode.com/"
+    employee = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    tasks = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [t.get("title") for t in tasks if t.get("completed")]
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee.get("name"), len(completed), len(tasks)))
+    [print("\t {}".format(c)) for c in completed]
