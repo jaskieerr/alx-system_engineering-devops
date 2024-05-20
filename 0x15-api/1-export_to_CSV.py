1ebd0d6855811e01fa6111922c96d0e130c304f7
jaskieerr <nizar.zairat@gmail.com>
2024-05-20 16:04:09 -0700

    testu

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''fetches emloyee stuff'''
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    employee = requests.get(url + "employee/{}".format(sys.argv[1])).json()
+    tasks = requests.get(url + "todos", params={"employee id": sys.argv[1]}).json()
+
+    completed = [t.get("title") for t in tasks if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee.get("name"), len(completed), len(tasks)))
+    [print("\t {}".format(c)) for c in completed]
