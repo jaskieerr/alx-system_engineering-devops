520bff02bd01dca9438e0e6c30baddfd67633440
jaskieerr <nizar.zairat@gmail.com>
2024-06-18 06:19:05 -0700

    ss

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ import requests
 def number_of_subscribers(subreddit):
     ''''cheks how many subs'''
     api_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    res = requests.get(api_url, headers={'User-agent': 'usea'})
+    res = requests.get(api_url, headers={'User-agent': 'testu'})
     if res.status_code == 200:
         return res.json().get('data').get('subscribers')
     else:
