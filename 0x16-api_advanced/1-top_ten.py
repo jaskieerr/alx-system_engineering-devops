8bf6f548706bea8688c4dccd9c353bd8a5aef7f0
jaskieerr <nizar.zairat@gmail.com>
2024-06-18 06:20:19 -0700

    ss

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Top Ten'''
+
+import requests
+
+
+def top_ten(subreddit):
+    ''' prints titles of first 10 hot posts listed for a subreddit'''
+    api_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+    res = requests.get(api_url, headers={'User-agent': 'anothertestu'},
+                        allow_redirects=False)
+    if res.status_code == 200:
+        posts = res.json().get('data').get('children')
+        count = 0
+        for post in posts:
+            if count == 10:
+                break
+            print(post.get("data").get("title"))
+            count = count + 1
+    else:
+        print("None")
