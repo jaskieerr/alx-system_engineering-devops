07bae0c1c9dc9eb6ddde670ab4cbc5d9b47185f0
jaskieerr <nizar.zairat@gmail.com>
2024-08-22 07:36:48 -0700

    fingers crossed

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-# Change nginx file limit
+# updating limit
 exec { 'Change nginx limit':
   command  => 'sudo sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart',
   provider => shell,
